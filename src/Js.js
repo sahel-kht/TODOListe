49dc8153f3070f2a521665d5e9270175d9ce1736
doaaa <doaa.alhajali.2001.26@gmail.com>
2023-08-27 14:03:45 +0200

    Neu

--- src/Js.js
+++ src/Js.js
@@ -1,4 +1,26 @@
-function löschen(){
+// Js.js
 
+function hinzufuegen() {
+    var taskInput = document.getElementById("Hinzufügen");
+    var taskText = taskInput.value;
 
+    if (taskText !== "") {
+        var taskList = document.getElementById("task-list");
+
+        var li = document.createElement("li");
+        li.textContent = taskText;
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Löschen";
+        deleteButton.className = "delete";
+        deleteButton.addEventListener("click", function () {
+            taskList.removeChild(li);
+        });
+
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+
+        taskInput.value = "";
+        taskInput.focus();
+    }
 }
